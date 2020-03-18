54354d4bb83c21f42835a5ae5a024f5423616691
troyyxk <xingkunyin@hotmail.com>
2020-03-17 23:34:21 -0400

    Add graph to svc and adaboost.

--- q3_2_2.py
+++ q3_2_2.py
@@ -5,6 +5,51 @@ from sklearn.model_selection import KFold
 from sklearn.svm import LinearSVC
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, roc_curve
 import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+
+def labels_to_one_hot(labels):
+    targets = []
+    for lbl in labels:
+        a = [0.]*10
+        a[int(lbl)] = 1.
+        targets.append(a)
+    return np.array(targets)
+
+def plot_roc(y_test, y_score):
+    n_classes = 10
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    # First aggregate all false positive rates
+    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+    # Then interpolate all ROC curves at this points
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(n_classes):
+        mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+    # Finally average it and compute AUC
+    mean_tpr /= n_classes
+
+    lw = 2
+    plt.figure()
+
+    colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+    for i, color in zip(range(n_classes), colors):
+        plt.plot(fpr[i], tpr[i], color=color, lw=lw,
+                label='ROC curve of class {0} (area = {1:0.2f})'
+                ''.format(i, roc_auc[i]))
+
+    plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC graph of Adaboost')
+    plt.legend(loc="lower right")
+    plt.show()
 
 def main():
     print("Enter main.")
@@ -12,12 +57,21 @@ def main():
 
     clf = LinearSVC(random_state=0, tol=1e-5)
     clf.fit(train_data, train_labels)
+
     prediction = clf.predict(test_data)
+    y_test = labels_to_one_hot(test_labels)
+
+    # prediction_proba = clf.predict_proba(test_data)  # (#, 10), for each
+    prediction_proba = clf.fit(train_data, train_labels).decision_function(test_data)
+
+    plot_roc(y_test, prediction_proba)
+
     print("Accuracy: ", clf.score(test_data, test_labels))
     print("confusion matrix: ")
     print(confusion_matrix(test_labels, prediction))
     print("Precision: ", precision_score(test_labels, prediction, average='macro'))
     print("Recall: ", recall_score(test_labels, prediction, average='macro'))
+
 
 if __name__ == '__main__':
     main()
--- q3_2_3.py
+++ q3_2_3.py
@@ -5,6 +5,51 @@ from sklearn.model_selection import KFold
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, roc_curve
 import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+
+def labels_to_one_hot(labels):
+    targets = []
+    for lbl in labels:
+        a = [0.]*10
+        a[int(lbl)] = 1.
+        targets.append(a)
+    return np.array(targets)
+
+def plot_roc(y_test, y_score):
+    n_classes = 10
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    # First aggregate all false positive rates
+    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+    # Then interpolate all ROC curves at this points
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(n_classes):
+        mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+    # Finally average it and compute AUC
+    mean_tpr /= n_classes
+
+    lw = 2
+    plt.figure()
+
+    colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+    for i, color in zip(range(n_classes), colors):
+        plt.plot(fpr[i], tpr[i], color=color, lw=lw,
+                label='ROC curve of class {0} (area = {1:0.2f})'
+                ''.format(i, roc_auc[i]))
+
+    plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC graph of Adaboost')
+    plt.legend(loc="lower right")
+    plt.show()
 
 def main():
     print("Enter main.")
@@ -12,7 +57,13 @@ def main():
 
     clf = AdaBoostClassifier(n_estimators=1000, random_state=0, learning_rate=0.9, algorithm='SAMME')
     clf.fit(train_data, train_labels)
+
     prediction = clf.predict(test_data)
+
+    prediction_proba = clf.predict_proba(test_data)  # (#, 10), for each
+    y_test = labels_to_one_hot(test_labels)
+    plot_roc(y_test, prediction_proba)
+
     print("Accuracy: ", clf.score(test_data, test_labels))
     print("confusion matrix: ")
     print(confusion_matrix(test_labels, prediction))
